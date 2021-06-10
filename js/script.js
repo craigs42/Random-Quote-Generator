8279021f58d03d70028920165668f21c1e25a172
Craig <craigschauer@gmail.com>
2021-06-09 23:05:13 -0700

    Objects, printQuote, getRandomNum255
    Added Objects and properties, added printQuote(), added getRandomNum255 for background color

--- js/script.js
+++ js/script.js
@@ -3,19 +3,39 @@
 ***/
 const quotes = [
   {
-
+    quote : 'hi',
+    source: 'unknown',
+    citation: 'unknown',
+    year: 1975,
+    tags: 'rock'
   },
   {
-
+    quote : 'hi',
+    source: 'unknown',
+    citation: 'unknown',
+    year: 1975,
+    tags: 'rock'
   },
   {
-
+    quote : 'hi',
+    source: 'unknown',
+    citation: 'unknown',
+    year: 1975,
+    tags: 'rock'
   },
   {
-
+    quote : 'hi',
+    source: 'unknown',
+    citation: 'unknown',
+    year: 1975,
+    tags: 'rock'
   },
   {
-
+    quote : 'hi',
+    source: 'unknown',
+    citation: 'unknown',
+    year: 1975,
+    tags: 'rock'
   }
 ];
 
@@ -27,12 +47,18 @@ function getRandomQuote(){
 
 }
 
+function getRandomNum255(){
+  const num = Math.floor(Math.random() * 255);
+  console.log(num);
+  return num;
+}
 
 /***
  * `printQuote` function
 ***/
 function printQuote(){
-
+  const body = document.querySelector('body');
+  body.style.backgroundColor = `rgb(${getRandomNum255()},${getRandomNum255()},${getRandomNum255()})`;
 }
 
 

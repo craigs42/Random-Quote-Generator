4bd46497c60693765f32cfb00926309aebb82f08
Craig <craigschauer@gmail.com>
2021-06-09 22:36:13 -0700

    Structure
    structure of the random quote generator

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote(){
+
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote(){
+
+}
+
+
+/***
+ * click event listener for the print quote button
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
